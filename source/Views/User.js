5ea4cca4a04309335847ff7dad9c227dc2ea7c9f
Mati en Qubit <mrussitto@qubit.tv>
2017-06-26 17:46:05 -0300

    comments + user

--- source/Views/User.js
+++ source/Views/User.js
@@ -3,15 +3,15 @@ import u from '../utils.js'
 
 export default  {
   loading: true,
-  items: [],
+  user: false,
   oninit: function () {
     this.loading = true
     var user = m.route.param('user')
     var url = config.base + 'user/' + user
     var self = this
     m.request(url)
-    .then(function (items) {
-      self.items = items
+    .then(function (user) {
+      self.user = user
       self.loading = false
     })
   },
@@ -19,6 +19,12 @@ export default  {
     if (this.loading) {
       return m('.loading')
     }
-    return u.items2dom(this.items)
+    u.log(this.user)
+    return [
+      m('h4', this.user.id),
+      m('.karma', 'karma: ' + this.user.karma),
+      m('.time', 'created: ' + this.user.created),
+      m.trust(this.user.about)
+    ]
   }
 }
--- source/utils.js
+++ source/utils.js
@@ -1,5 +1,11 @@
+var log = console.log.bind(console) // eslint-disable-line no-console
+
 export default {
-  items2dom: items2dom
+  items2dom: items2dom,
+  comms2dom: comms2dom,
+  adClass: adClass,
+  rmClass: rmClass,
+  log: log
 }
 
 function items2dom(items) {
@@ -7,31 +13,92 @@ function items2dom(items) {
   // use the for young padawan to go faster
   for (var i = 0, l = items.length; i < l; i++) {
     var item = items[i]
-    var comm = '#!/comments/' + item.id
     var title = item.title
     if (item.domain) {
       title += ' (' + item.domain + ')'
     }
-    var points = m('span.badge[data-badge=' + item.points + ']', 'Points')
-    if (item.points === null) {
-      points = ''
+
+    var subline = []
+    if (item.points !== null) {
+      subline.push(m('span.badge[data-badge=' + item.points + ']', 'Points'))
     }
-    var st = [
+    subline.push(m('span.badge[data-badge=' + item.comments_count + ']',
+      m('a[href=#!/item/' + item.id + ']', 'Comments')))
+    if (item.user) {
+      subline.push(m('span.user', m('a[href=#!/user/' + item.user + ']', 'by ' + item.user)))
+    }
+    subline.push(m('span.time', item.time_ago))
+    var columns = []
+    for (var j = 0, ll = subline.length; j < ll; j++) {
+      columns.push(m('.column.col-' + (12/ll), subline[j]))
+    }
+
+    var st = m('.news-item', [
       m('h5',
         m('a[href=' + item.url + '][target=_blank][rel=noopener]', title)),
-      m('.columns', [
-        m('.column.col-3', points),
-        m('.column.col-3',
-          m('span.badge[data-badge=' + item.comments_count + ']', m('a[href=' + comm + ']', 'Comments'))),
-        m('.column.col-3',
-          // m('span.user', m('a[href=#!/user/' + item.user + ']', 'by ' + item.user))),
-          m('span.user', 'by ' + item.user)),
-        m('.column.col-3',
-          m('span.time', item.time_ago)),
-      ]),
+      m('.columns', columns),
       m('.divider')
-    ]
+    ])
     ret.push(st)
   }
   return ret
+}
+
+function comms2dom(comms) {
+  var ret = []
+  // use the for young padawan to go faster
+  for (var i = 0, l = comms.length; i < l; i++) {
+    var comm = comms[i]
+    var content = comm.content
+    var elDom = [
+      m.trust(content),
+      m('.stats', comm.time_ago + ' by ' + comm.user)
+    ]
+    if (comm.comments.length) {
+      elDom.push(comms2dom(comm.comments))
+    }
+    if (comm.level == 0) {
+      elDom.push(m('.divider'))
+    }
+    ret.push(m('.comment[style=margin-left: ' + (comm.level*2) + 'rem]', elDom))
+  }
+  return ret
+}
+
+function adClass(el, clas) {
+  var actual = el.className.trim()
+  if (actual == '') {
+    el.className = clas
+    return 1
+  }
+  if (actual == clas) {
+    return 0
+  }
+  if (actual.indexOf(' ' + clas) >= 0 || actual.indexOf(clas + ' ') >= 0) {
+    return 0
+  }
+  el.className += ' ' + clas
+  return 1
+}
+
+function rmClass(el, clas) {
+  var actual = el.className.trim()
+  if (actual == '') {
+    return 0
+  }
+  if (actual == clas) {
+    el.className = ''
+    return 0
+  }
+  var t = ' ' + clas
+  if (actual.indexOf(t) >= 0) {
+    el.className = el.className.replace(t, '')
+    return 1
+  }
+  t = clas + ' '
+  if (actual.indexOf(t) >= 0) {
+    el.className = el.className.replace(t, '')
+    return 1
+  }
+  return 0
 }
