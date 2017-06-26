badd431f50c8eec378561eaa83fd4ff29dd4a9c5
Mati en Qubit <mrussitto@qubit.tv>
2017-06-26 10:16:44 -0300

    worker + manifest relative paths

--- source/sw.js
+++ source/sw.js
@@ -1,21 +1,20 @@
 var CACHE_NAME = 'cache-v0'
 var urlsToCache = [
-  '/',
-  '/index.html',
-  '/main.js',
+  'index.html',
+  'main.js',
   'https://cdnjs.cloudflare.com/ajax/libs/spectre.css/0.2.14/spectre.min.css',
   'https://unpkg.com/mithril@1.1.1/mithril.min.js',
-  '/img/icon.png',
-  '/img/icon-256.png',
-  '/img/icon-128.png',
-  '/img/icon-64.png',
-  '/img/icon-32.png',
+  'img/icon.png',
+  'img/icon-256.png',
+  'img/icon-128.png',
+  'img/icon-64.png',
+  'img/icon-32.png',
   'https://api.hackerwebapp.com/news?page=1',
   'https://api.hackerwebapp.com/newest?page=1',
   'https://api.hackerwebapp.com/ask?page=1',
   'https://api.hackerwebapp.com/show?page=1',
   'https://api.hackerwebapp.com/jobs?page=1',
-  '/favicon.ico',
+  'favicon.ico',
 ]
 
 self.addEventListener('install', function(event) {
