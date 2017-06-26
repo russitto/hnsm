16bb10200086bff7aa056096a3e14ad31069df51
Mati en Qubit <mrussitto@qubit.tv>
2017-06-26 10:11:42 -0300

    sw.js relative path

--- source/main.js
+++ source/main.js
@@ -2,7 +2,7 @@ var root = document.getElementById('container')
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
