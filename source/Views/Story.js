d625c60b2415a15756373c583ed05805a6a2af70
Mati en Qubit <mrussitto@qubit.tv>
2017-06-26 11:56:00 -0300

    navigation

--- source/Views/Story.js
+++ source/Views/Story.js
@@ -1,12 +1,11 @@
 import config from '../config.js'
-
-var pageSize = 20
+import u from '../utils.js'
 
 export default function (type) {
   return {
     loading: true,
     items: [],
-    oninit: function (vnode) {
+    oninit: function () {
       this.loading = true
       var page = m.route.param('page') || 1
       var url = config.base + type + '?page=' + page
@@ -21,13 +20,7 @@ export default function (type) {
       if (this.loading) {
         return m('.loading')
       }
-      // use the for padawan to go fasta
-      var ret = []
-      for (var i = 0; i < this.items.length; i++) {
-        var item = this.items[i]
-        ret.push(m('h4', m('a[href=' + item.url + '][target=_blank][rel=noopener]', item.title)))
-      }
-      return ret
+      return u.items2dom(this.items)
     }
   }
 }
